c16276dfb8a67bae91dc008128e5925b23594d38
domanh09012006 <doducmanh09012006@gmail.com>
2024-12-21 10:30:14 +0700

    BAITAPTONGHOP.cpp

--- BAITAPTONGHOP.cpp
+++ BAITAPTONGHOP.cpp
@@ -60,18 +60,18 @@ int main(){
 				if(pos<0 || pos>n){
 					printf("Khong co mon an nay");
 				}else{
-							printf("Sua ten mon an: ");
-							fgets(newmember,sizeof(newmember),stdin);
-							newmember[strcspn(newmember,"\n")]='\0';
-							printf("Sua gia mon an: ");
-							scanf("%d", &newprice);
-							fflush(stdin);
-							strcpy(DishMenu[pos].member,newmember);
-							DishMenu[pos].price=newprice;
-							for(int i=0; i<n; i++){
-							printf("\n%d. %s: %d",DishMenu[i].STT, DishMenu[i].member, DishMenu[i].price);
-							}
-						}
+					printf("Sua ten mon an: ");
+					fgets(newmember,sizeof(newmember),stdin);
+					newmember[strcspn(newmember,"\n")]='\0';
+					printf("Sua gia mon an: ");
+					scanf("%d", &newprice);
+					fflush(stdin);
+					strcpy(DishMenu[pos].member,newmember);
+					DishMenu[pos].price=newprice;
+					for(int i=0; i<n; i++){
+					printf("\n%d. %s: %d",DishMenu[i].STT, DishMenu[i].member, DishMenu[i].price);
+					}
+				}
 			break;
 			case 4://Xoa mon an cuoi menu
 				if(n>0){
@@ -85,33 +85,33 @@ int main(){
 			break;
 			case 5://Sap xep tang dan mon an theo gia
 				for (int i=0; i<n-1; i++) {
-                    for (int j=i+1; j<n; j++) {
-                        if (DishMenu[i].price>DishMenu[j].price) {
-                            struct Menu temp = DishMenu[i];
-                            DishMenu[i] = DishMenu[j];
-                            DishMenu[j] = temp;
-                        }
-                    }
-                }
-                printf("Mon an sau khi sap xep la: ");
-                for(int i=0; i<n; i++){
+		                    for (int j=i+1; j<n; j++) {
+		                        if (DishMenu[i].price>DishMenu[j].price) {
+		                            struct Menu temp = DishMenu[i];
+		                            DishMenu[i] = DishMenu[j];
+		                            DishMenu[j] = temp;
+		                        }
+		                    }
+		                }
+			        printf("Mon an sau khi sap xep la: ");
+			        for(int i=0; i<n; i++){
 					printf("\n%d. %s: %d",DishMenu[i].STT, DishMenu[i].member, DishMenu[i].price);
 				}
 			break;
 			case 6://Tim mon an
 				printf("Nhap ten mon an can tim: ");
-                fgets(newmember, sizeof(newmember), stdin);
-                newmember[strcspn(newmember, "\n")] = '\0';
-                printf("Ket qua tim kiem:\n");
-                for (int i=0; i<n; i++) {
-                    if (strstr(DishMenu[i].member, newmember)) {
-                        printf("Mon an co trong DishMenu");
-                        found=1;
-                    }
-                }
-                if (!found) {
-                    printf("Khong tim thay mon an!\n");
-                }
+		                fgets(newmember, sizeof(newmember), stdin);
+		                newmember[strcspn(newmember, "\n")] = '\0';
+		                printf("Ket qua tim kiem:\n");
+		                for (int i=0; i<n; i++) {
+		                    if (strstr(DishMenu[i].member, newmember)) {
+		                        printf("Mon an co trong DishMenu");
+		                        found=1;
+		                    }
+		                }
+		                if (!found) {
+		                    printf("Khong tim thay mon an!\n");
+		                }
 			break;
 			case 7://Thoat
 				printf("Thoat!");
